f328fb6e98cd543804ebaa5c9b049f9b5fb1c0fc
Auliyauswatunkhasanah <auliyauswatunn@gmail.com>
2020-11-20 19:43:23 +0700

    Add project files.

---
+++ FirstWebWithWCF/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstWebWithWCF
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        ServiceReference1.Service1Client Client = new ServiceReference1.Service1Client();
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            TextBox3.Text = Client.add(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text)).ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = Client.Sub(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text)).ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = Client.Multipli(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text)).ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            TextBox3.Text = Client.Divide(double.Parse(TextBox1.Text), double.Parse(TextBox2.Text)).ToString();
+        }
+    }
+}
